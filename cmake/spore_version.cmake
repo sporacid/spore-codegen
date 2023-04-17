ccdc3897882d7e718e134b1204b981cd3e2ba681
sporacid <sporacid@gmail.com>
2023-04-17 17:56:38 -0400

    fix: Fix semantic versioning not resetting minor and patch versions accordingly

--- cmake/spore_version.cmake
+++ cmake/spore_version.cmake
@@ -7,7 +7,7 @@ function(spore_get_version SPORE_VERSION_OUT)
 
   if (GIT_FOUND)
     execute_process(
-      COMMAND ${GIT_EXECUTABLE} --no-pager log --format=%H
+      COMMAND ${GIT_EXECUTABLE} --no-pager log --format=%H --reverse
       WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
       OUTPUT_VARIABLE SPORE_GIT_COMMITS
     )
@@ -30,8 +30,11 @@ function(spore_get_version SPORE_VERSION_OUT)
 
       if (${SPORE_GIT_COMMIT_BODY} MATCHES ".*BREAKING CHANGES:.*$" OR ${SPORE_GIT_COMMIT_BODY} MATCHES "^[a-zA-Z0-9-_]+(\([a-zA-Z0-9-_]+\))?!:")
         math(EXPR SPORE_VERSION_MAJOR "${SPORE_VERSION_MAJOR}+1")
+        set(SPORE_VERSION_MINOR 0)
+        set(SPORE_VERSION_PATCH 0)
       elseif (${SPORE_GIT_COMMIT_BODY} MATCHES "^feat(\([a-zA-Z0-9-_]+\))?:")
         math(EXPR SPORE_VERSION_MINOR "${SPORE_VERSION_MINOR}+1")
+        set(SPORE_VERSION_PATCH 0)
       elseif (${SPORE_GIT_COMMIT_BODY} MATCHES "^fix(\([a-zA-Z0-9-_]+\))?:")
         math(EXPR SPORE_VERSION_PATCH "${SPORE_VERSION_PATCH}+1")
       endif ()
