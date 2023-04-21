e173af035b2e9d6644c837b30408c3d0f3f2913c
sporacid <sporacid@gmail.com>
2023-04-21 18:11:35 -0400

    fixup! feat: Add chaiscript as a scripting prototype for rendering

---
+++ include/spore/codegen/scripts/codegen_script.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <string>
+#include <functional>
+
+#include "nlohmann/json.hpp"
+
+namespace spore::codegen
+{
+    #if 0
+    struct codegen_script_function
+    {
+        using func_type = std::function<bool(const std::vector<nlohmann::json>&)>;
+        std::string name;
+        func_type func;
+    };
+    #endif
+
+    using codegen_script_function = std::function<bool(const std::vector<nlohmann::json>& args)>;
+
+    struct codegen_script
+    {
+        std::map<std::string, codegen_script_function> function_map;
+
+        bool invoke(const std::string& name, const std::vector<nlohmann::json>& args)
+        {
+            const auto it = function_map.find(name);
+            return it != function_map.end() ? (*it)(args) : false;
+        }
+    };
+}
