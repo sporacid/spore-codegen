5709197cd46ee3a49bcf5efd53cd8c576b13a919
sporacid <sporacid@gmail.com>
2023-08-15 22:43:14 -0400

    fixup! chore: Sets fast pre-processing flag

--- include/spore/codegen/codegen_config.hpp
+++ include/spore/codegen/codegen_config.hpp
@@ -8,12 +8,19 @@
 
 namespace spore::codegen
 {
+
+    struct codegen_config_pass
+    {
+        std::vector<std::string> templates;
+        std::optional<std::string> output;
+        std::optional<nlohmann::json> condition;
+    };
+
     struct codegen_config_step
     {
         std::string name;
         std::string input;
-        std::vector<std::string> templates;
-        std::optional<nlohmann::json> condition;
+        std::vector<codegen_config_pass> passes;
     };
 
     struct codegen_config
@@ -21,15 +28,41 @@ namespace spore::codegen
         std::vector<codegen_config_step> steps;
     };
 
-    void from_json(const nlohmann::json& json, codegen_config_step& value)
+    void from_json(const nlohmann::json& json, codegen_config_pass& value)
     {
-        json["name"].get_to(value.name);
-        json["input"].get_to(value.input);
         json["templates"].get_to(value.templates);
+
+        if (json.contains("output"))
+        {
+            json["output"].get_to(value.output.emplace());
+        }
 
         if (json.contains("condition"))
         {
             json["condition"].get_to(value.condition.emplace());
+        }
+    }
+
+    void from_json(const nlohmann::json& json, codegen_config_step& value)
+    {
+        json["name"].get_to(value.name);
+        json["input"].get_to(value.input);
+
+        if (json.contains("passes"))
+        {
+            // v2
+            json["passes"].get_to(value.passes);
+        }
+        else
+        {
+            // v1
+            codegen_config_pass& pass = value.passes.emplace_back();
+            json["templates"].get_to(pass.templates);
+
+            if (json.contains("condition"))
+            {
+                json["condition"].get_to(pass.condition.emplace());
+            }
         }
     }
 
