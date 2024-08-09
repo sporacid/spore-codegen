193503bd4d814fbba2539fe4d5835d1adc2ab8c7
sporacid <sporacid@gmail.com>
2024-08-09 15:11:54 -0400

    feat: Adds YAML and BSON support for JSON files used by the executable
    ## Application
    - Adds support for YAML and BSON format for everything that requires a JSON file
    - Adds auto-detection of file format for JSON-compatible types
    - Fix hashes which were not applied on binary files
    
    ## Build
    - Fix VERSION file not being found by CMake

--- include/spore/codegen/utils/files.hpp
+++ include/spore/codegen/utils/files.hpp
@@ -8,13 +8,63 @@
 
 #include "picosha2.h"
 
+#include "spore/codegen/utils/yaml.hpp"
+
 namespace spore::codegen::files
 {
-    inline bool write_file(const std::string_view& path, const std::string& content)
+    namespace detail
     {
-        std::filesystem::path parent = std::filesystem::path(path).parent_path();
+        enum class json_file_type
+        {
+            none,
+            json,
+            bson,
+            yaml,
+        };
 
-        if (!parent.empty() && !std::filesystem::exists(parent) && !std::filesystem::create_directories(parent))
+        inline json_file_type get_json_type(std::string_view path)
+        {
+            constexpr std::string_view json_exts[] {".json"};
+            constexpr std::string_view bson_exts[] {".bson"};
+            constexpr std::string_view yaml_exts[] {".yml", ".yaml"};
+
+            std::string ext = std::filesystem::path(path).extension().string();
+            const auto predicate = [&](std::string_view json_ext) { return json_ext == ext; };
+
+            if (std::any_of(std::begin(json_exts), std::end(json_exts), predicate))
+            {
+                return json_file_type::json;
+            }
+
+            if (std::any_of(std::begin(bson_exts), std::end(bson_exts), predicate))
+            {
+                return json_file_type::bson;
+            }
+
+            if (std::any_of(std::begin(yaml_exts), std::end(yaml_exts), predicate))
+            {
+                return json_file_type::yaml;
+            }
+
+            return json_file_type::none;
+        }
+
+        inline bool create_directories(std::string_view path)
+        {
+            std::filesystem::path parent = std::filesystem::path(path).parent_path();
+
+            if (!parent.empty() && !std::filesystem::exists(parent) && !std::filesystem::create_directories(parent))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+    inline bool write_file(std::string_view path, const std::string& content)
+    {
+        if (!detail::create_directories(path))
         {
             return false;
         }
@@ -25,7 +75,43 @@ namespace spore::codegen::files
         return !stream.bad();
     }
 
-    inline bool read_file(const std::string_view& path, std::string& content)
+    inline bool write_file(std::string_view path, const std::vector<std::uint8_t>& bytes)
+    {
+        if (!detail::create_directories(path))
+        {
+            return false;
+        }
+
+        std::ofstream stream(path.data(), std::ios::out | std::ios::binary);
+        stream.write(reinterpret_cast<const char*>(bytes.data()), static_cast<std::streamsize>(bytes.size()));
+        stream.close();
+        return !stream.bad();
+    }
+
+    inline bool write_file(std::string_view path, const nlohmann::json& json)
+    {
+        constexpr std::size_t indent = 2;
+        switch (detail::get_json_type(path))
+        {
+            case detail::json_file_type::json: {
+                return write_file(path, json.dump(indent));
+            }
+
+            case detail::json_file_type::bson: {
+                return write_file(path, nlohmann::json::to_bson(json));
+            }
+
+            case detail::json_file_type::yaml: {
+                return write_file(path, yaml::to_yaml(json, indent));
+            }
+
+            default: {
+                return false;
+            }
+        }
+    }
+
+    inline bool read_file(std::string_view path, std::string& content)
     {
         std::ifstream stream(path.data());
         if (!stream.is_open())
@@ -39,15 +125,75 @@ namespace spore::codegen::files
         return !stream.bad();
     }
 
-    inline bool hash_file(const std::string_view& path, std::string& hash)
+    inline bool read_file(std::string_view path, std::vector<std::uint8_t>& bytes)
     {
-        std::string file_content;
-        if (!read_file(path, file_content))
+        std::ifstream stream(path.data(), std::ios::in | std::ios::binary);
+        if (!stream.is_open())
         {
             return false;
         }
 
-        hash = picosha2::hash256_hex_string(file_content.begin(), file_content.end());
+        bytes = {
+            std::istreambuf_iterator<char>(stream),
+            std::istreambuf_iterator<char>(),
+        };
+
+        stream.close();
+        return !stream.bad();
+    }
+
+    inline bool read_file(std::string_view path, nlohmann::json& json)
+    {
+        json = nlohmann::json(nlohmann::json::value_t::discarded);
+        switch (detail::get_json_type(path))
+        {
+            case detail::json_file_type::json: {
+                std::string value;
+                if (read_file(path, value))
+                {
+                    json = nlohmann::json::parse(value, nullptr, false, false);
+                }
+
+                break;
+            }
+
+            case detail::json_file_type::bson: {
+                std::vector<std::uint8_t> value;
+                if (read_file(path, value))
+                {
+                    json = nlohmann::json::from_bson(value, true, false);
+                }
+
+                break;
+            }
+
+            case detail::json_file_type::yaml: {
+                std::string value;
+                if (read_file(path, value))
+                {
+                    json = yaml::from_yaml(value);
+                }
+
+                break;
+            }
+
+            default: {
+                break;
+            }
+        }
+
+        return !json.is_discarded();
+    }
+
+    inline bool hash_file(std::string_view path, std::string& hash)
+    {
+        std::vector<std::uint8_t> data;
+        if (!read_file(path, data))
+        {
+            return false;
+        }
+
+        picosha2::hash256_hex_string(data.begin(), data.end(), hash);
         return true;
     }
 }
