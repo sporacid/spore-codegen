406b551e7600615f037c93bf96a2193eee3aead1
sporacid <sporacid@gmail.com>
2024-08-10 22:45:49 -0400

    chore: Improve find scripts to only log messages once and allow CMake variable for LLVM_DIR variable

--- cmake/FindClangFormat.cmake
+++ cmake/FindClangFormat.cmake
@@ -1,7 +1,15 @@
-if (DEFINED ENV{LLVM_DIR})
+include(FindPackageMessage)
+
+if (DEFINED LLVM_DIR)
+  set(_LLVM_DIR ${LLVM_DIR})
+elseif (DEFINED ENV{LLVM_DIR})
+  set(_LLVM_DIR $ENV{LLVM_DIR})
+endif ()
+
+if (DEFINED _LLVM_DIR)
   set(
     CLANG_EXECUTABLE_PATHS
-    $ENV{LLVM_DIR}/bin
+      ${_LLVM_DIR}/bin
   )
 endif ()
 
@@ -13,12 +21,23 @@ find_program(
 
 if (CLANG_FORMAT_EXECUTABLE)
   set(CLANG_FORMAT_FOUND TRUE)
-  message(STATUS "Clang format was found, executable=${CLANG_FORMAT_EXECUTABLE}")
+  find_package_message(
+    ClangFormat
+    "Clang format was found, executable=${CLANG_FORMAT_EXECUTABLE}"
+    "[${CLANG_FORMAT_EXECUTABLE}]"
+  )
 else ()
   set(CLANG_FORMAT_FOUND FALSE)
   if (CLANG_FORMAT_REQUIRED)
     message(FATAL_ERROR "Clang format was not found")
-  else()
-    message(STATUS "Clang format was not found")
+  else ()
+    find_package_message(
+      Clang
+      "Clang format was not found, directory=${_LLVM_DIR}"
+      "[${_LLVM_DIR}]"
+    )
   endif ()
 endif ()
+
+unset(_LLVM_DIR)
+unset(CLANG_EXECUTABLE_PATHS)
