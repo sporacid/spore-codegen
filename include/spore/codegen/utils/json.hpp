a6c576baadeaed95c64983ab7c23290ebdf56c72
sporacid <sporacid@gmail.com>
2025-08-13 15:48:50 -0400

    feat: Add format function

--- include/spore/codegen/utils/json.hpp
+++ include/spore/codegen/utils/json.hpp
@@ -92,3 +92,82 @@ namespace spore::codegen::json
         }
     }
 }
+
+template <>
+struct std::formatter<nlohmann::json>
+{
+    std::string format_string;
+
+    auto parse(std::format_parse_context& ctx)
+    {
+        const auto it_end = std::find(ctx.begin(), ctx.end(), '}');
+        const std::string_view format_args {ctx.begin(), it_end};
+
+        format_string.resize(0);
+        format_string += "{";
+
+        if (not format_args.empty())
+        {
+            format_string += ":";
+            format_string += format_args;
+        }
+
+        format_string += "}";
+
+        return it_end;
+    }
+
+    auto format(const nlohmann::json& json, std::format_context& ctx) const -> decltype(ctx.out())
+    {
+        switch (json.type())
+        {
+            case nlohmann::json::value_t::array: {
+                auto out = ctx.out();
+                out = std::format_to(out, "[");
+
+                const std::vector<nlohmann::json>& json_values = json.get_ref<const std::vector<nlohmann::json>&>();
+                for (const nlohmann::json& json_value : json_values)
+                {
+                    ctx.advance_to(out);
+                    out = format(json_value, ctx);
+                }
+
+                out = std::format_to(out, "]");
+                return out;
+            }
+
+            case nlohmann::json::value_t::string: {
+                const std::string& value = json.get_ref<const std::string&>();
+                return std::vformat_to(ctx.out(), format_string, std::make_format_args(value));
+            }
+
+            case nlohmann::json::value_t::object: {
+                const std::string value = json.dump();
+                return std::vformat_to(ctx.out(), format_string, std::make_format_args(value));
+            }
+
+            case nlohmann::json::value_t::number_integer: {
+                const std::int64_t value = json.get<std::int64_t>();
+                return std::vformat_to(ctx.out(), format_string, std::make_format_args(value));
+            }
+
+            case nlohmann::json::value_t::number_unsigned: {
+                const std::uint64_t value = json.get<std::uint64_t>();
+                return std::vformat_to(ctx.out(), format_string, std::make_format_args(value));
+            }
+
+            case nlohmann::json::value_t::number_float: {
+                const std::double_t value = json.get<std::double_t>();
+                return std::vformat_to(ctx.out(), format_string, std::make_format_args(value));
+            }
+
+            case nlohmann::json::value_t::boolean: {
+                const bool value = json.get<bool>();
+                return std::vformat_to(ctx.out(), format_string, std::make_format_args(value));
+            }
+
+            default:
+                return ctx.out();
+        }
+    }
+};
